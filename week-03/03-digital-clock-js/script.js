d93993caafab8c08aaada10a4cf3bbf92393a82b
RDOToole89 <roibinotoole@gmail.com>
2021-11-19 18:13:31 +0100

    digital clock functionality working

---
+++ week-03/03-digital-clock-js/script.js
@@ -0,0 +1,37 @@
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+
+const setClock = () => {
+  const currentDate = new Date();
+  const currentHour = currentDate.getHours();
+  const currentMinute = currentDate.getMinutes();
+  const currentSecond = currentDate.getSeconds();
+
+  hoursEl.textContent = currentHour;
+  minutesEl.textContent = currentMinute * 1;
+  secondsEl.textContent = currentSecond;
+};
+
+setClock();
+
+let timer = null;
+
+const startClock = () => {
+  if (timer) {
+    timer = null;
+  } else {
+    timer = setInterval(setClock, 1000);
+  }
+};
+
+start.addEventListener('click', startClock);
+
+stop.addEventListener('click', () => {
+  if (timer) {
+    clearInterval(timer);
+    timer = null;
+  }
+});
