ac887e753646678ad8bb855f0152caed1451a5ff
RDOToole89 <roibinotoole@gmail.com>
2021-11-30 14:06:02 +0100

    clock v1 done

--- 03 - projects/03-clock/dist/js/script.js
+++ 03 - projects/03-clock/dist/js/script.js
@@ -1,1 +1,45 @@
-console.log('Hello from the console');
+const hours = document.querySelectorAll('.clock__hour');
+const hourNumber = document.querySelectorAll('.clock__number');
+const hourHand = document.querySelector('.clock__hand--hour');
+const minuteHand = document.querySelector('.clock__hand--minute');
+const secondHand = document.querySelector('.clock__hand--second');
+
+// Rotates the hours with x deg for each number
+const rotateHours = () => {
+  let rotation = 29.6;
+
+  hours.forEach((hour) => {
+    hour.style.transform = `rotate(${rotation}deg)`;
+    rotation += 29.6;
+  });
+
+  rotation = 29.6;
+
+  hourNumber.forEach((hourNumber, i) => {
+    hourNumber.style.transform = `rotate(${-rotation}deg)`;
+
+    rotation += 29.6;
+  });
+};
+
+rotateHours();
+
+// Function that takes in a clock hand a time ratio
+const setHand = (element, ratio) => {
+  const rotation = ratio * 360;
+  element.style.transform = `rotate(${rotation}deg)`;
+};
+
+const startClock = () => {
+  const currentDate = new Date();
+
+  const secondRatio = currentDate.getSeconds() / 60;
+  const minuteRatio = (secondRatio + currentDate.getMinutes()) / 60;
+  const hourRatio = (minuteRatio + currentDate.getHours()) / 12;
+
+  setHand(secondHand, secondRatio);
+  setHand(minuteHand, minuteRatio);
+  setHand(hourHand, hourRatio);
+};
+
+let start = setInterval(startClock, 1000);
