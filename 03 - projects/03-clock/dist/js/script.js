5a9b1e5f2dc389e7d6c288f1f3f282799f68b63c
RDOToole89 <roibinotoole@gmail.com>
2021-11-30 18:27:45 +0100

    add comments to clock

--- 03 - projects/03-clock/dist/js/script.js
+++ 03 - projects/03-clock/dist/js/script.js
@@ -12,43 +12,50 @@ const minuteDigitEl = document.querySelector('.clock__minute-digit');
 const secondDigitEl = document.querySelector('.clock__second-digit');
 const miliSecondDigitEl = document.querySelector('.clock__milisecond-digit');
 
-// Rotates the hours with x deg for each number
-const rotateHours = () => {
-  let rotation = 29.72;
+// Rotates the hours with x degrees for each hour number
+const rotateHours = (degrees) => {
+  let rotation = degrees;
 
   hours.forEach((hour) => {
     hour.style.transform = `rotate(${rotation}deg)`;
-    rotation += 29.72;
+    rotation += degrees;
   });
 
-  rotation = 29.72;
+  // Resets the rotation for the next part of the function which
+  // rotates the number itself on its x-axis
+  rotation = degrees;
 
   hourNumber.forEach((hourNumber, i) => {
     hourNumber.style.transform = `rotate(${-rotation}deg)`;
 
-    rotation += 29.72;
+    rotation += degrees;
   });
 };
 
-rotateHours();
+rotateHours(29.71);
 
-const rotateSegments = () => {
-  let rotation = 30;
+// Rotates the the segments with x degrees for each 5 minute segment
+const rotateSegments = (degrees) => {
+  let rotation = degrees;
 
   segments.forEach((segment) => {
     console.log(segment);
     segment.style.transform = `rotate(${rotation}deg)`;
-    rotation += 30;
+    rotation += degrees;
   });
 };
 
-rotateSegments();
+rotateSegments(30);
 
-// Function that takes in a clock hand a time ratio
+// Function that takes in a clock hand and a time ratio
 const setHand = (element, ratio) => {
   const rotation = ratio * 360;
   element.style.transform = `rotate(${rotation}deg)`;
 };
+
+// Function that takes in a number and prepends a '0' to it
+// if the number is smaller than 10. This is to simulate
+// digital clock notatation
 
 const digitToString = (number) => {
   if (number < 10) {
@@ -58,6 +65,8 @@ const digitToString = (number) => {
   return number.toString();
 };
 
+// Function to set the digits on a digital clock type interface
+// takes in an element and a timeUnit
 const setDigit = (element, timeUnit) => {
   const currentDate = new Date();
   const currentMiliSeconds = digitToString(currentDate.getMilliseconds());
@@ -88,22 +97,19 @@ const setDigit = (element, timeUnit) => {
   }
 };
 
+// Starts the clock and sets the clock to its correct positions
 const startClock = () => {
   const currentDate = new Date();
   const currentDay = currentDate.getDay();
   const currentMonth = currentDate.getMonth();
   const currentYear = currentDate.getFullYear();
 
-  // Sets the date
-
-  day.innerText = '0' + currentDay.toString();
-  month.innerText = currentMonth < 10 ? '0' + currentMonth.toString() : currentMonth;
-  year.innerText = currentYear;
+  // Sets the dates
+  day.innerText = digitToString(currentDay);
+  month.innerText = digitToString(currentMonth);
+  year.innerText = digitToString(currentYear);
 
   // Sets the hands
-
-  // Function that takes in a clock hand a time ratio
-
   const secondRatio = currentDate.getSeconds() / 60;
   const minuteRatio = (secondRatio + currentDate.getMinutes()) / 60;
   const hourRatio = (minuteRatio + currentDate.getHours()) / 12;
