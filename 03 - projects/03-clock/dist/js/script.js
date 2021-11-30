090ad967356cc835772682056a8dba7d0fbf10ed
RDOToole89 <roibinotoole@gmail.com>
2021-11-30 15:15:19 +0100

    add digital clock

--- 03 - projects/03-clock/dist/js/script.js
+++ 03 - projects/03-clock/dist/js/script.js
@@ -7,6 +7,10 @@ const segments = document.querySelectorAll('.clock__segment');
 const day = document.querySelector('.clock__day');
 const month = document.querySelector('.clock__month');
 const year = document.querySelector('.clock__year');
+const hourDigitEl = document.querySelector('.clock__hour-digit');
+const minuteDigitEl = document.querySelector('.clock__minute-digit');
+const secondDigitEl = document.querySelector('.clock__second-digit');
+const miliSecondDigitEl = document.querySelector('.clock__milisecond-digit');
 
 // Rotates the hours with x deg for each number
 const rotateHours = () => {
@@ -46,6 +50,32 @@ const setHand = (element, ratio) => {
   element.style.transform = `rotate(${rotation}deg)`;
 };
 
+const setDigit = (element, timeUnit) => {
+  const currentDate = new Date();
+
+  switch (timeUnit) {
+    case 'hours': {
+      element.innerText = currentDate.getHours();
+      break;
+    }
+    case 'minutes': {
+      element.innerText = currentDate.getMinutes();
+      break;
+    }
+    case 'seconds': {
+      element.innerText = currentDate.getSeconds();
+      break;
+    }
+    case 'miliSeconds': {
+      element.innerText = currentDate.getMilliseconds();
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+};
+
 const startClock = () => {
   const currentDate = new Date();
   const currentDay = currentDate.getDay();
@@ -62,6 +92,11 @@ const startClock = () => {
   const secondRatio = currentDate.getSeconds() / 60;
   const minuteRatio = (secondRatio + currentDate.getMinutes()) / 60;
   const hourRatio = (minuteRatio + currentDate.getHours()) / 12;
+
+  // setDigit(miliSecondDigitEl, 'miliSeconds');
+  setDigit(secondDigitEl, 'seconds');
+  setDigit(minuteDigitEl, 'minutes');
+  setDigit(hourDigitEl, 'hours');
 
   setHand(secondHand, secondRatio);
   setHand(minuteHand, minuteRatio);
