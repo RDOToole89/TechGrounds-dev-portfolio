b6896773eb4b3a1b93366c3eb452f85d8ced4a56
RDOToole89 <roibinotoole@gmail.com>
2021-12-07 16:50:25 +0100

    start section 11 of TS => create folder and basic typescript scaffolding

---
+++ 06-etc/03 - TypeScript/s11-reusable-code/src/index.ts
@@ -0,0 +1,41 @@
+// install the type definitions for the Node standard library npm i @types/node
+
+// Node module to access the file system
+import fs from 'fs';
+
+// loads in all the matches expecting 'utf-8' encoding as one giant string
+const matches = fs
+  .readFileSync('./src/football.csv', { encoding: 'utf-8' })
+  // splits each match on a \n new line into an array of match strings
+  .split('\n')
+  // maps over each string (row) and turns the data into an array per match
+  .map((row: string): string[] => {
+    // splits the data on the ,
+    return row.split(',');
+  });
+
+// console.log(matches);
+
+const homeWin = 'H';
+const awayWin = 'A';
+const draw = 'D';
+
+const MatchResult = {
+  HomeWin: 'H',
+  AwayWin: 'A',
+  Draw: 'D',
+};
+
+let manUnitedWins = 0;
+
+// [date, hometeam, awayteam, homescore, awayscore, winningteam, man of the match]
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === homeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === awayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games.`);
