2dc7caa633d40d716e4512e740bd867ac0461e15
RDOToole89 <roibinotoole@gmail.com>
2021-12-09 17:57:12 +0100

    add some more styling to challenge 5 modal

--- 01-paircoding-challanges/05-challenge-modal-finished/script.js
+++ 01-paircoding-challanges/05-challenge-modal-finished/script.js
@@ -4,7 +4,8 @@ const buttons = document.querySelectorAll('.btn');
 const buttonContainer = document.querySelector('.button-container');
 const overlay = document.querySelector('.overlay');
 
-// Loops over each button on the page and creates a modal based on the button data-set
+// Loops over each button on the page and creates a modal based on the button 'dataset' value.
+
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     const animalType = button.dataset['animal'];
