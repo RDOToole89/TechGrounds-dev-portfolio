73df672333788674536233a69243ade2c867a9d9
RDOToole89 <roibinotoole@gmail.com>
2021-12-13 21:35:26 +0100

    add populate function

--- 03 - projects/05-quiz-app/src/index.js
+++ 03 - projects/05-quiz-app/src/index.js
@@ -12,11 +12,17 @@ console.log(questionObject);
 
 const populate = () => {
   const questions = document.querySelectorAll('.quiz-question');
+  const question = document.querySelector('.quiz-top__display');
 
-  // [...questions].forEach((question, i) => {
-  //   question.textContent = questionObject[0].
-  // });
-  // console.log(questions);
+  question.textContent = questionObject[0].question;
+
+  [...questions].forEach((question, i) => {
+    const questionContent = [...question.children].find((el) =>
+      el.classList.contains('quiz-question__answer')
+    );
+    questionContent.innerText = questionObject[0].possibleAnswers[i];
+  });
+  console.log(questions);
 };
 
 populate();
