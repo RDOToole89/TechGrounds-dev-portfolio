64d8aab301616e1c5631c053d74a508e2e72897f
RDOToole89 <roibinotoole@gmail.com>
2021-12-23 17:13:23 +0100

    add comments to quiz for documentation

--- 01-projects/05-quiz-app/src/Question.js
+++ 01-projects/05-quiz-app/src/Question.js
@@ -1,26 +1,36 @@
 import { getRandomNumber, hasDuplicates, popRandom, stringToOperator } from './helper.js';
 
 export class Question {
+  // Constructor takes in a number for how many questions need to be generated.
   constructor(numberOfQuestions) {
     this.numberOfQuestions = numberOfQuestions;
   }
 
+  // Creates a MathQuestion Array => takes in an upperbound for the questions generated.
   createMathQuestion(numberUpperBound) {
+    // Possible operators to generate questions with.
     const operators = ['-', '+', '*'];
 
     const questionArray = [];
 
+    // Loop over for the number of questions that need to be generated.
     for (let i = 0; i < this.numberOfQuestions; i++) {
+      // Creates two numbers and grabs a random operator
       const numberOne = getRandomNumber(1, numberUpperBound);
       const numberTwo = getRandomNumber(1, numberUpperBound);
       const randomOperator = operators[getRandomNumber(1, operators.length)];
+
+      // Calculates the correct answer using stringToOperator helper function.
       let correctAnswerCalc = stringToOperator[randomOperator](numberOne, numberTwo);
 
+      // Creates a Math Object for each iteration of the loop and pushes it in to the questionArray.
       const mathQuestionObject = {
         number: i + 1,
         question: `Q: ${numberOne} ${randomOperator} ${numberTwo}`,
         correctAnswer: correctAnswerCalc,
+        // Creates an array with with possible Math Questions.
         possibleAnswers: this.createPossibleAnswers(randomOperator, correctAnswerCalc, 5),
+        // userAnswer is first set to null to signify no answer has yet been given.
         userAnswer: null,
       };
 
@@ -30,12 +40,15 @@ export class Question {
     return questionArray;
   }
 
+  // Creates a UNIQUE array with answers and then plugs the correct answer into the array.
   createPossibleAnswers(randomOperatorString, correctAnswer, numberOfPossibleAnswers) {
     let possibleAnswers = [];
 
     for (let i = 0; i < numberOfPossibleAnswers; i++) {
       switch (randomOperatorString) {
         case '-': {
+          // Calculates a possible answer that deviates with a margin
+          // between 1 and 20 from the correct answer
           const randomNumber = getRandomNumber(
             correctAnswer - getRandomNumber(1, 20),
             correctAnswer + getRandomNumber(1, 20)
@@ -65,17 +78,17 @@ export class Question {
       }
     }
 
-    // Creates an array of unique values by transforming it to a set and
+    // Creates an array of unique values by transforming it to a set.
     let possibleAnswersSet = new Set([...possibleAnswers]);
 
-    // Generates a new possible answer
+    // Generates a new possible answer.
     let newPossibleNumber = getRandomNumber(
       correctAnswer - getRandomNumber(1, 20),
       correctAnswer + getRandomNumber(1, 20)
     );
 
-    // Condition checks for if the possibleAnswers Array has the right length
-    // Does NOT contain duplicates and does NOT contain the right answer
+    // Condition checks for if the possibleAnswers Array has the right length and
+    // does NOT contain duplicates and does NOT contain the right answer.
     if (
       !hasDuplicates(possibleAnswers) &&
       possibleAnswers.length === numberOfPossibleAnswers &&
@@ -97,7 +110,7 @@ export class Question {
         correctAnswer + getRandomNumber(1, 20)
       );
 
-      // Checks if the a unique set has been created and contains the right answer if so it returns
+      // Checks if a unique set has been created and contains the right answer if so it returns.
       if (
         [...possibleAnswersSet].includes(correctAnswer) &&
         possibleAnswersSet.size === numberOfPossibleAnswers
@@ -107,7 +120,7 @@ export class Question {
         return possibleAnswers;
       }
 
-      // If the set is unique but does not contain the correct answer insert it and return
+      // If the set is unique but does not contain the correct answer insert it and return.
       if (
         possibleAnswersSet.size === numberOfPossibleAnswers &&
         ![...possibleAnswersSet].includes(correctAnswer)
@@ -118,6 +131,8 @@ export class Question {
         return possibleAnswers;
       }
     }
+
+    // If the array already has the correct answer and the correct size return.
     return possibleAnswers;
   }
 }
