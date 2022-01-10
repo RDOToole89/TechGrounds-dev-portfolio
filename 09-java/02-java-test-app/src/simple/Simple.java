b632c35c823dbf44eb7087d143d0f9c931aa111d
RDOToole89 <roibinotoole@gmail.com>
2022-01-10 22:15:33 +0100

    add folder for java

---
+++ 09-java/02-java-test-app/src/simple/Simple.java
@@ -0,0 +1,10 @@
+
+// package simple;
+
+public class Simple {
+  String message = "Random Message";
+
+  public static void main(String args[]) {
+    System.out.println("Welcome to package");
+  }
+}
